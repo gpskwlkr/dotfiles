cd0c8d0f91c4bd5880231036c23787e827da0e6b
gpskwlkr <giorgi.anakidze@outlook.com>
2023-11-14 13:05:24 +0400

    dotfiles

---
+++ nvim/lua/gpskwlkr/plugins/init.lua
@@ -0,0 +1,6 @@
+return {
+    "christoomey/vim-tmux-navigator",
+    "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+    "inkarkat/vim-ReplaceWithRegister",
+}
